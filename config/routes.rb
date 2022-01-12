3a9f69cbacf338e4356d2655c4e8c00dedff6b88
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-01-12 13:52:46 +0000

    [Add]画像投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: "homes#top"
+  
+  resources :post_images, only: [:new, :create, :index, :show]
+  
   get 'homes/about' => 'homes#index',as: 'about'
   
 end
