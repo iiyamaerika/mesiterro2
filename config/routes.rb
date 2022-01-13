1c449fb5b4b035904926599a219747efdad2c56e
iiyamaerika <95344302+iiyamaerika@users.noreply.github.com>
2022-01-13 05:17:47 +0000

    [Add]4-15 ユーザー詳細画面

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "homes#top"
-  
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  
   get 'homes/about' => 'homes#index',as: 'about'
-  
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  resources :users, only: [:show, :edit]  
 end
